87963a0fe9fd1084d4c85c03bbca12ff45159ee2
ubinzat <ubinzat@gmail.com>
2023-12-14 02:19:49 +0300

    Update frb.R

--- R/frb.R
+++ R/frb.R
@@ -45,7 +45,7 @@
 #
 #' @export
 frb <- function(lmrob.object, nboot=1000, return.coef = FALSE, 
-                return.indices = FALSE, centered=TRUE,zzz=1) {
+                return.indices = FALSE, centered=TRUE) {
   zz=1
   lmrob.Chi <- Mchi
   lmrob.Psi <- Mpsi
