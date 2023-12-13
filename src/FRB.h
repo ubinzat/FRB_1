5b1891f0b803945ca355b1a4b5479a9450a93967
ubinzat <ubinzat@gmail.com>
2023-12-14 02:10:59 +0300

    Update FRB.h

--- src/FRB.h
+++ src/FRB.h
@@ -13,6 +13,6 @@
 #define dngettext(pkg, String, StringP, N) (N > 1 ? StringP : String)
 #endif
   
-void R_frb(double *xx, double *y, double *w, int *n, int *p, double *beta_m,
-            double *scale, double *chi_res_s, double *bbetas, int *nboot,
-            double *xx3, double *v2, int *bind);
+void R_frb( double *xx, double *y, double *w, int *n, int *p, double *beta_m,
+		double *scale, double *chi_res_s, double *bbetas, int *nboot,
+		double *xx3, double *v2, int *bind, double *zz);
